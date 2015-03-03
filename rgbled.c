f404121f98cc1022700e03da802504992886e5e9
Mathew Richard Rumsey <mathew.rumsey@maine.edu>
2015-03-03 20:05:47 +0000

    Added list of abbreviations. Successfully compiled and ran module (prints on insmod and rmmod)

--- rgbled.c
+++ rgbled.c
@@ -14,4 +14,39 @@
  * appropriate permissions.
  */
 
+#include <linux/module.h>	/* Need */
+#include <linux/kernel.h>	/* Need */
+#include <linux/init.h>		/* Allow rename init and exit functions */
+#include <linux/stat.h>		/* Permissions */
+#include <linux/fs.h>		/* Required for device drivers */
+#include <linux/types.h>	/* Required for dev_t (device numbers) */
+#include <linux/spinlock.h>	/* Required for spinlocks */
+#include <linux/ioctl.h>	/* Required for ioctl commands */
+#include <linux/delay.h>	/* Required for udelay */
+#include <asm/uaccess.h>	/* Move data to and from user-space */
+/* #include <asm/semaphore.h>	 Required for use of semaphores */
+/* #include <linux/tty.h>	 used for console_print() */
 
+#define RGBLED_MOD_AUTHOR Rumsey
+#define RGBLED_MOD_DESCR XXXX
+#define RGBLED_MOD_SDEV SHEAFF_XMEGA_RPi_EXPANSION_FG
+
+MODULE_LICENSE("GPL");
+/*MODULE_AUTHOR(RGBLED_MOD_AUTHOR);
+MODULE_DESCRIPTION(RGBLED_MOD_DESCR);
+MODULE_SUPPORTED_DEVICE(RGBLED_MOD_SDEV);
+*/
+
+static int led_init(void)
+{
+	printk(KERN_ALERT "LED here!\n");
+	return 0;
+}
+
+static void led_exit(void)
+{
+	printk(KERN_ALERT "LED leaving!\n");
+}
+
+module_init(led_init);
+module_exit(led_exit);
