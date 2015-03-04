229558c12bd79c79a8dc589eb2d8479d697ad1bd
Mathew Richard Rumsey <mathew.rumsey@maine.edu>
2015-03-04 01:15:49 +0000

    Added constant defines and global progress bools
    Prototyped functions and rearranged file to match linux kernel coding styles
    
    Began work on _init and _exit

--- rgbled.c
+++ rgbled.c
@@ -1,3 +1,4 @@
+/* vim: set tabstop=4 softtabstop=0 noexpandtab shiftwidth=4 : */
 /* 
  * Author: Mathew Rumsey
  * Date: 03 02 15
@@ -23,30 +24,66 @@
 #include <linux/spinlock.h>	/* Required for spinlocks */
 #include <linux/ioctl.h>	/* Required for ioctl commands */
 #include <linux/delay.h>	/* Required for udelay */
+#include <linux/errno.h>	/* Set error messages */
 #include <asm/uaccess.h>	/* Move data to and from user-space */
 /* #include <asm/semaphore.h>	 Required for use of semaphores */
 /* #include <linux/tty.h>	 used for console_print() */
 
-#define RGBLED_MOD_AUTHOR Rumsey
-#define RGBLED_MOD_DESCR XXXX
+/* Defines for module */
+#define RGBLED_MOD_AUTHOR RUMSEY
+#define RGBLED_MOD_DESCR DEVICE_DRIVER_FOR_SHEAFFS_XMEGA_RPi_EXPANSION_FG
 #define RGBLED_MOD_SDEV SHEAFF_XMEGA_RPi_EXPANSION_FG
+/* Defines for constants */
+#define COUNT 1
+#define FIRST 0
+#define NAME "rgbled"
+#define MODE 222
+
+/* Used to keep track of registrations to handle cleanup */
+bool alloc_chrdev = false;
+
+/* Other necessary global variables */
+dev_t dev;
+
+/* Function list */
+static int __init led_init(void);
+static void led_exit(void);
+
+int led_init(void)
+{
+	int err = 0;
+
+/* ========== END VARIABLE LIST ========== */
+
+	printk(KERN_ALERT "LED here!\n");
+
+	/* Allocate dynamic device number */
+	if ((err = alloc_chrdev_region(&dev, FIRST, COUNT, NAME))) {
+		printk(KERN_ERR "E ALLOCATING DEVICE NUMBERS : %d", -err);
+		led_exit();
+		return -err;
+	} else
+		alloc_chrdev = true;
+	
+	/* Create device files */
+
+	return 0;
+}
+
+void led_exit(void)
+{
+	printk(KERN_ALERT "LED leaving!\n");
+
+	if (alloc_chrdev)
+		unregister_chrdev_region(dev, COUNT);
+	
+}
+
+module_init(led_init);
+module_exit(led_exit);
 
 MODULE_LICENSE("GPL");
 /*MODULE_AUTHOR(RGBLED_MOD_AUTHOR);
 MODULE_DESCRIPTION(RGBLED_MOD_DESCR);
 MODULE_SUPPORTED_DEVICE(RGBLED_MOD_SDEV);
 */
-
-static int led_init(void)
-{
-	printk(KERN_ALERT "LED here!\n");
-	return 0;
-}
-
-static void led_exit(void)
-{
-	printk(KERN_ALERT "LED leaving!\n");
-}
-
-module_init(led_init);
-module_exit(led_exit);
