d411b8229d5680f3fe365570b1f4dcac3da0d1d0
Mathew Richard Rumsey <mathew.rumsey@maine.edu>
2015-03-21 22:02:57 +0000

    Added forking to suite.pl to increase spam rate of ./test

--- suite.pl
+++ suite.pl
@@ -3,14 +3,23 @@ use strict;
 use warnings;
 
 # Get proper arguments
-die "Enter integer argument for number of executions\n" unless $#ARGV == 0; 
-my $executions = $ARGV[0];
+die "Enter 2 integer arguments for number of threads & executions/thread\n" 
+							unless $#ARGV == 1; 
+my $forks = $ARGV[0] - 1;
+my $executions = $ARGV[1];
+
+# Fork to number of threads requested
+my $pid = 0;
+for my $j (1..$forks) {
+	$pid = fork() if $pid == 0;
+}
 
 my ($rr, $rg, $rb);
 my $output;
 
 # Spam ./test with random values the number of times indicated from argument
-for my $i (1..$executions) {
+my $i = 0;
+for $i (1..$executions) {
 	$rr = int(rand(2048));
 	$rg = int(rand(2048));
 	$rb = int(rand(2048));
@@ -18,9 +27,11 @@ for my $i (1..$executions) {
 	$output = `./test $rr $rg $rb 2>&1`;
 	# Report if error
 	if ($output ne "") { 
-		print "EXECUTION $i ERROR:\n";
+		print "EXECUTION $i ERROR FOR PID $pid:\n";
 		printf "%04d : %04d : %04d -> %s\n", $rr, $rg, $rb, $output;
 		# Clear error for next execution
 		$output = "";
 	}
 }
+
+print "PID $pid did $i executions\n";
