964b6ad0069b232c941d1ee2713a8fbc70434bc8
Mathew Richard Rumsey <mathew.rumsey@maine.edu>
2015-03-21 21:26:43 +0000

    FIXED suite.pl error-message/exucution# catching.
    Added comments to suite.pl

--- suite.pl
+++ suite.pl
@@ -2,18 +2,25 @@
 use strict;
 use warnings;
 
+# Get proper arguments
 die "Enter integer argument for number of executions\n" unless $#ARGV == 0; 
 my $executions = $ARGV[0];
 
 my ($rr, $rg, $rb);
 my $output;
+
+# Spam ./test with random values the number of times indicated from argument
 for my $i (1..$executions) {
 	$rr = int(rand(2048));
 	$rg = int(rand(2048));
 	$rb = int(rand(2048));
-	$output = `./test $rr $rg $rb`;
+	# Execute ./test and make sure to capture STDIN and STDERR
+	$output = `./test $rr $rg $rb 2>&1`;
+	# Report if error
 	if ($output ne "") { 
-		print "EXECUTION $executions ERROR:\n";
+		print "EXECUTION $i ERROR:\n";
 		printf "%04d : %04d : %04d -> %s\n", $rr, $rg, $rb, $output;
+		# Clear error for next execution
+		$output = "";
 	}
 }
